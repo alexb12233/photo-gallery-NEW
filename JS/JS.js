d44f7c05d81347e6ee32920313f880959ac34f02
CODE <alexbluelandrover@gmail.com>
2025-01-05 11:59:02 +0000

    Add files via upload

---
+++ JS/JS.js
@@ -0,0 +1,4 @@
+function first() {
+    document.getElementById("image").src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/autumn-in-the-white-mountains-of-new-hampshire-royalty-free-image-841380450-1531931081.jpg?crop=1.00xw:0.755xh;0,0.202xh&resize=1200"
+    document.getElementById("season").innerHTML="autumn"
+}
