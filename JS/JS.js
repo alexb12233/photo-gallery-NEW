10acc3258b4d673b2f0c0f5c1e687a98354f14ca
CODE <alexbluelandrover@gmail.com>
2025-02-02 11:17:32 +0000

    Add files via upload

--- JS/JS.js
+++ JS/JS.js
@@ -2,3 +2,13 @@ function first() {
     document.getElementById("image").src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/autumn-in-the-white-mountains-of-new-hampshire-royalty-free-image-841380450-1531931081.jpg?crop=1.00xw:0.755xh;0,0.202xh&resize=1200"
     document.getElementById("season").innerHTML="autumn"
 }
+
+function second() {
+    document.getElementById("image").src="https://i.natgeofe.com/k/20b88422-ae27-4b5d-af43-9eb76a23a477/WH-Solstice_4x3.jpg"
+    document.getElementById("season").innerHTML="winter"
+}
+
+function third() {
+    document.getElementById("image").src="https://images.squarespace-cdn.com/content/v1/5811566d20099e23814644fd/1580754598184-ATYRJ25IPF6ZSV87J851/early-spring-care-for-flowering-trees-shrubs-ornamental-cherry.jpg"
+    document.getElementById("season").innerHTML="spring"
+}
